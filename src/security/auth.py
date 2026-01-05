4d7947db0a5520e3533f7082e2b1e01bfcb9c507
sjr2003 <sajjadrajabi.2003@gmail.com>
2026-01-05 09:49:37 +0330

    Extremely simple auth for signaling server testing

--- src/security/auth.py
+++ src/security/auth.py
@@ -4,3 +4,12 @@ Roadmap:
 - Validates UI clients
 - Supports token-based auth
 """
+
+
+def verify_user(user_name: str, password: str) ->bool:
+    key_pass = "admin"
+
+    if user_name == key_pass and password == key_pass:
+        return True
+    else:
+        return False
